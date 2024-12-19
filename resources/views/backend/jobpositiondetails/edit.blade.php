10b5373d852a313e6720396ad90b941cba18cabc
govind663 <whitdevil06@gmail.com>
2024-12-19 14:30:25 +0530

    Damian-Corporate | Version - 0.22

--- resources/views/backend/jobpositiondetails/edit.blade.php
+++ resources/views/backend/jobpositiondetails/edit.blade.php
@@ -95,9 +95,9 @@ Damian Corporate | Edit Job Position
                         @enderror
                     </div>
 
-                    <label class="col-sm-2"><b>Responsibilities : </b></label>
+                    <label class="col-sm-2"><b>Job Location Link : </b></label>
                     <div class="col-sm-4 col-md-4">
-                        <input type="text" name="job_location" id="job_location" class="form-control @error('job_location') is-invalid @enderror" value="{{ $jobpositiondetails->job_location }}" placeholder="Enter Project Location Link.">
+                        <input type="text" name="job_location" id="job_location" class="form-control @error('job_location') is-invalid @enderror" value="{{ $jobpositiondetails->job_location }}" placeholder="Enter Job Location Link.">
                         @error('job_location')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
