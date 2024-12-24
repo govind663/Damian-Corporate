43103ee8d574f3b1060c2e205d4833c885053070
govind663 <whitdevil06@gmail.com>
2024-12-24 17:59:32 +0530

    Damian-Corporate | Version - 0.54

--- app/Http/Controllers/frontend/HomeController.php
+++ app/Http/Controllers/frontend/HomeController.php
@@ -27,7 +27,7 @@ class HomeController extends Controller
 
         $testimonials = Testimonial::orderBy("id","desc")->where('status',1)->whereNull('deleted_at')->get();
 
-        $projects = Project::with('category')->orderBy("id","desc")->where('status',1)->whereNull('deleted_at')->paginate(9);
+        $projects = Project::with('category')->orderBy("id","asc")->where('status',1)->whereNull('deleted_at')->paginate(9);
 
         $ourServices = OurServices::orderBy("id","asc")->where('status',1)->whereNull('deleted_at')->get();
 
