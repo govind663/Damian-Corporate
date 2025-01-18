919db1e44c95a8cf827c64916011791110fa74fe
govind663 <whitdevil06@gmail.com>
2025-01-18 07:47:58 +0530

    damian-corporate | Version - 0.21

--- app/View/Components/backend/MainJs.php
+++ app/View/Components/backend/MainJs.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\View\Components\Backend;
+namespace App\View\Components\backend;
 
 use Closure;
 use Illuminate\Contracts\View\View;
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '6ccf45469bc4bf1e877fecb4f5524dc94a369302',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -395,9 +395,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6ccf45469bc4bf1e877fecb4f5524dc94a369302',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
